d2a6ab3545480a97dece2318204790a914befba2
hcervino <haritz.cervino@gmail.com>
2014-06-28 13:09:46 +0200

    Comments and Readme.md

--- AP_Baro_MPL115A2.cpp
+++ AP_Baro_MPL115A2.cpp
@@ -1,5 +1,14 @@
 /// -*- tab-width: 4; Mode: C++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+/************************************************************************************/
+/*!
+	@file 	AP_Baro_MPL115A2.cpp
+	@author hcervino 
+	@date 	28/06/2014
 
+	Ardupilot/Beaglepilot driver for the MPL115A2 barometric pressure sensor 
+	based in the Adafruit_MPL115A2 driver for Arduino(K.Townsend (Adafruit Industries)
+*/
+/************************************************************************************/
 
 // AVR LibC Includes
 #include <inttypes.h>
@@ -31,6 +40,7 @@ bool AP_Baro_MPL115A2::init()
         
     // Read factory coefficient values (this only needs to be done once)
     if (hal.i2c->readRegisters(MPL115A2_ADDRESS, MPL115A2_REGISTER_A0_COEFF_MSB, 8, buff) != 0) {
+        hal.i2c->setHighSpeed(false);
         healthy = false;
         i2c_sem->give();
         return false;
@@ -39,13 +49,16 @@ bool AP_Baro_MPL115A2::init()
 	a0coeff = (( (uint8_t) buff[0] << 8) | buff[1]);
 	b1coeff = (( (uint8_t) buff[2] << 8) | buff[3]);
 	b2coeff = (( (uint8_t) buff[4] << 8) | buff[5]);
-	c12coeff = (( (uint8_t) (buff[6] << 8) | buff[7])) >> 2;
+	c12coeff = (( (uint8_t) (buff[6] << 8) | buff[7])) >> 2;	
 	
 	_mpl115a2_a0 = (float)a0coeff / 8;
 	_mpl115a2_b1 = (float)b1coeff / 8192;
 	_mpl115a2_b2 = (float)b2coeff / 16384;
 	_mpl115a2_c12 = (float)c12coeff;
 	_mpl115a2_c12 /= 4194304.0;	
+	
+	_last_press_read_command_time = 0;
+    _last_temp_read_command_time = 0;
 		
 	healthy = true;
     i2c_sem->give();
@@ -67,18 +80,8 @@ float AP_Baro_MPL115A2::get_temperature() {
 
 // Read the sensor. This is a state machine
 // acumulate a new sensor reading
-void AP_Baro_MPL115A2::accumulate(void)
-{
-	// get pointer to i2c bus semaphore
-    AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
-    
-     // take i2c bus sempahore
-    if (!i2c_sem->take(1))
-        return;
-     
-    getPT(&Press, &Temp);   
-    
-    i2c_sem->give();    
+void AP_Baro_MPL115A2::accumulate(void){     
+    getPT(&Press, &Temp);         
 }
 
 // Read the sensor using accumulated data
@@ -112,6 +115,13 @@ void AP_Baro_MPL115A2::getPT(float *P, float *T)
 	uint16_t pressure, temp;
 	float pressureComp;
 	uint8_t buff[4];
+	
+	// get pointer to i2c bus semaphore
+    AP_HAL::Semaphore* i2c_sem = hal.i2c->get_semaphore();
+    
+     // take i2c bus sempahore
+    if (!i2c_sem->take(1))
+        return;
            
 	// Send command STARTCONVERSION	
 	if(hal.i2c->writeRegister(MPL115A2_ADDRESS, MPL115A2_REGISTER_STARTCONVERSION, 0x00)!=0){
@@ -120,10 +130,15 @@ void AP_Baro_MPL115A2::getPT(float *P, float *T)
 	
 	// Wait a bit for the conversion to complete (3ms max)
 	hal.scheduler->delay(5);
-
+	
+	 if (!healthy && hal.scheduler->millis() < _retry_time) {
+        return; 
+     }   
+		
 	// Get raw pressure and temperature settings
 	if(hal.i2c->readRegisters(MPL115A2_ADDRESS, MPL115A2_REGISTER_PRESSURE_MSB, 4, buff)!=0){
 		_retry_time = hal.scheduler->millis() + 1000;
+		hal.i2c->setHighSpeed(false);
 		healthy=false;
 		return;
 	}	
@@ -140,6 +155,7 @@ void AP_Baro_MPL115A2::getPT(float *P, float *T)
 	*P = ((65.0F / 1023.0F) * pressureComp) + 50.0F; // kPa
 	*T = ((float) temp - 498.0F) / -5.35F +25.0F; // C
 	
+	//Accumulate data
 	_press_sum +=*P;
 	_temp_sum +=*T;
 	
@@ -149,4 +165,6 @@ void AP_Baro_MPL115A2::getPT(float *P, float *T)
         _press_sum *= 0.5;
         _count /= 2;
     }
+    
+    i2c_sem->give();
 }
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ ardupilot_mpl115a2_driver
 =========================
 
 MPL115A2 driver for ardupilot
+
+Ardupilot/Beaglepilot driver for the MPL115A2 barometric pressure sensor 
+based in the Adafruit_MPL115A2 driver for Arduino(K.Townsend (Adafruit Industries))
+
+Set the two files (AP_Baro_MPL115A2.h, AP_Baro_MPL115A2.cpp) in the
+"ardupilot/libraries/AP_Baro" directory, then compile.
